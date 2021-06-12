3beaeb0f6a4e417bd196d98975e02bfd6b6dcc7a
Advorange <advorange@live.com>
2021-06-12 00:56:12 -0700

    Changed the inner dictionary in TypeRegistry to IDictionary instead of Dictionary.

--- src/YACCS/TypeRegistry.cs
+++ src/YACCS/TypeRegistry.cs
@@ -8,12 +8,12 @@ namespace YACCS
 	public abstract class TypeRegistry<T> : IDictionary<Type, T>, IReadOnlyDictionary<Type, T>
 	{
 		public int Count => Items.Count;
-		public bool IsReadOnly => ((ICollection<KeyValuePair<Type, T>>)Items).IsReadOnly;
+		public bool IsReadOnly => Items.IsReadOnly;
 		public ICollection<Type> Keys => Items.Keys;
 		IEnumerable<Type> IReadOnlyDictionary<Type, T>.Keys => Keys;
 		public ICollection<T> Values => Items.Values;
 		IEnumerable<T> IReadOnlyDictionary<Type, T>.Values => Values;
-		protected Dictionary<Type, T> Items { get; }
+		protected IDictionary<Type, T> Items { get; }
 
 		public virtual T this[Type key]
 		{
@@ -21,7 +21,7 @@ namespace YACCS
 			set => Items[key] = value;
 		}
 
-		protected TypeRegistry(Dictionary<Type, T> items)
+		protected TypeRegistry(IDictionary<Type, T> items)
 		{
 			Items = items;
 		}
@@ -36,13 +36,13 @@ namespace YACCS
 			=> Items.Clear();
 
 		public bool Contains(KeyValuePair<Type, T> item)
-			=> ((ICollection<KeyValuePair<Type, T>>)Items).Contains(item);
+			=> Items.Contains(item);
 
 		public bool ContainsKey(Type key)
 			=> Items.ContainsKey(key);
 
 		public void CopyTo(KeyValuePair<Type, T>[] array, int arrayIndex)
-			=> ((ICollection<KeyValuePair<Type, T>>)Items).CopyTo(array, arrayIndex);
+			=> Items.CopyTo(array, arrayIndex);
 
 		public IEnumerator<KeyValuePair<Type, T>> GetEnumerator()
 			=> Items.GetEnumerator();
@@ -51,7 +51,7 @@ namespace YACCS
 			=> Items.Remove(key);
 
 		public bool Remove(KeyValuePair<Type, T> item)
-			=> ((ICollection<KeyValuePair<Type, T>>)Items).Remove(item);
+			=> Items.Remove(item);
 
 		public abstract bool TryGetValue(Type key, [NotNullWhen(true)] out T value);
 
